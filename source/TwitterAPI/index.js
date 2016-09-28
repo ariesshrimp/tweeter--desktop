cc9c75c36482991991cf083977e207a0cd4cd5d8
joefraley <jose.fraley@gmail.com>
2016-09-28 13:11:03 -0700

    Hide API keys in a config file.

--- source/TwitterAPI/index.js
+++ source/TwitterAPI/index.js
@@ -1,27 +1,16 @@
 'use strict'
 import Twitter from 'twit'
 import Firebase from 'firebase'
-
+import Config from './config.json'
 
 /* API console: https://apps.twitter.com/app/12881539/show
 */
-const twitter = new Twitter({
-  consumer_key: 'L6Ky0Ap1KkZeZxJAOvazq8pwo',
-  consumer_secret: '214fCtbl8YX1UDwRdRkHhWGwZUcnhCjoj3Jw5GKU5OWZfvMm7w',
-  access_token: '2981248609-5PxgWjsFXfvYlhGE2me9GFHEqF2u9pQrGLHKI2Z',
-  access_token_secret: 'rWnAbvpV69P5uwVmsNm2SWILxk6EEtadaugvdYJl4uvrw'
-})
+const twitter = new Twitter(Config.twitter)
 
 
 /* API console: https://console.firebase.google.com/project/tweeter-80d2b/database/data
 */
-const app = Firebase.initializeApp({
-  apiKey: 'AIzaSyCtWEs8Hkc-5ysra-uDNBBCVIfZ-PdPRiw',
-  authDomain: 'tweeter-80d2b.firebaseapp.com',
-  databaseURL: 'https://tweeter-80d2b.firebaseio.com',
-  storageBucket: 'tweeter-80d2b.appspot.com',
-  messagingSenderId: '720389042280'
-})
+const app = Firebase.initializeApp(config.firebase)
 
 
 /* @returns Firebase Promise of <Void>
