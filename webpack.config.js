25bf37e691cfdbd1b7b907d6476cd3f44ecd719c
joefraley <jose.fraley@gmail.com>
2016-10-02 00:21:13 -0700

    Cleanups for webpack.config (no dev-server).

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,13 @@
 
 module.exports = {
   entry: './source/entry.js',
-  output: {
-    path: __dirname,
-    filename: 'bundle.js'
-  },
-  resolve: {
-    modules: [ 'node_modules' ]
-  },
+  output: { path: __dirname, filename: 'bundle.js' },
+  resolve: { modules: [ 'node_modules' ] },
   target: 'node',
   module: {
     loaders: [
       { test: /.js$/, exclude: /node_modules/, loader: 'babel'},
       { test: /.json$/, loader: 'json'}
     ]
-  },
-  devServer: {
-    contentBase: '/'
   }
 }
